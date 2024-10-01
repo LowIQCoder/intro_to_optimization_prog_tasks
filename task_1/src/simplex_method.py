dad69ab4f79c6fc275702dcdbe9afb3f3efb3e08
Nikita <nik.lizard.mobile@gmail.com>
2024-10-01 14:32:55 +0300

    Fix bug with negative coefficients and accuracy

--- task_1/src/simplex_method.py
+++ task_1/src/simplex_method.py
@@ -4,10 +4,8 @@ class SimplexMethod:
     ) -> tuple[list[float], float]:
         """
         Solves the Linear Programming problem using the Simplex method.
-
         Returns:
-            A tuple containing the decision variables and the optimal value.
-
+            A tuple containing variables optimal values and the optimal value of the objective function.
         Raises:
             Exception: If the method is not applicable.
         """
@@ -17,7 +15,7 @@ class SimplexMethod:
         self.accuracy = accuracy
         self.num_constraints = len(A)
         self.num_variables = len(C)
-        self.tableau = self.__initialize_tableau()
+        self.table = self.__initialize_table()
 
         if any(rhs < 0 for rhs in self.b):
             raise Exception("The method is not applicable!")
@@ -29,26 +27,28 @@ class SimplexMethod:
 
             pivot_row = self.__find_pivot_row(pivot_col)
             if pivot_row is None:
-                raise Exception("The problem is unbounded")
+                raise Exception("The method is not applicable!")
 
             self.__pivot(pivot_row, pivot_col)
 
         solution = self.__extract_solution()
-        optimal_value = self.tableau[-1][-1]
-        return solution, optimal_value
+        return solution, self.table[-1][-1]
 
-    def __initialize_tableau(self) -> list[list[float]]:
-        """Initializes the simplex tableau with slack variables."""
-        tableau = [
-            [element if element > self.accuracy else 0.0 for element in row]
+    def __initialize_table(self) -> list[list[float]]:
+        """Initializes the simplex table with slack variables."""
+        table = [
+            [
+                0.0 if -self.accuracy <= element <= self.accuracy else element
+                for element in row
+            ]
             + [0] * self.num_constraints
             + [rhs]
             for row, rhs in zip(self.A, self.b)
         ]
         for i in range(self.num_constraints):
-            tableau[i][self.num_variables + i] = 1
-        tableau.append([-c for c in self.C] + [0] * self.num_constraints + [0])
-        return tableau
+            table[i][self.num_variables + i] = 1
+        table.append([-c for c in self.C] + [0] * self.num_constraints + [0])
+        return table
 
     def __find_pivot_col(self) -> int | None:
         """
@@ -57,8 +57,8 @@ class SimplexMethod:
         Returns:
             The index of the pivot column or None if optimal.
         """
-        for j in range(len(self.tableau[-1]) - 1):
-            if self.tableau[-1][j] < 0:
+        for j in range(len(self.table[-1]) - 1):
+            if self.table[-1][j] < 0:
                 return j
         return None
 
@@ -75,35 +75,35 @@ class SimplexMethod:
         min_ratio = float("inf")
         pivot_row = None
         for i in range(self.num_constraints):
-            element = self.tableau[i][pivot_col]
-            if element > self.accuracy:
-                ratio = self.tableau[i][-1] / element
-                if ratio < min_ratio:
-                    min_ratio = ratio
-                    pivot_row = i
+            element = self.table[i][pivot_col]
+            if -self.accuracy <= element <= self.accuracy:
+                continue
+            ratio = self.table[i][-1] / element
+            if ratio < min_ratio:
+                min_ratio = ratio
+                pivot_row = i
         return pivot_row
 
     def __pivot(self, pivot_row: int, pivot_col: int):
         """
-        Performs the pivot operation to update the tableau.
+        Performs the pivot operation to update the table.
 
         Parameters:
             pivot_row: The index of the pivot row.
             pivot_col: The index of the pivot column.
         """
-        pivot_element = self.tableau[pivot_row][pivot_col]
-        self.tableau[pivot_row] = [x / pivot_element for x in self.tableau[pivot_row]]
-        for i in range(len(self.tableau)):
+        pivot_element = self.table[pivot_row][pivot_col]
+        self.table[pivot_row] = [x / pivot_element for x in self.table[pivot_row]]
+        for i in range(len(self.table)):
             if i != pivot_row:
-                factor = self.tableau[i][pivot_col]
-                self.tableau[i] = [
-                    x - factor * y
-                    for x, y in zip(self.tableau[i], self.tableau[pivot_row])
+                factor = self.table[i][pivot_col]
+                self.table[i] = [
+                    x - factor * y for x, y in zip(self.table[i], self.table[pivot_row])
                 ]
 
     def __extract_solution(self) -> list[float]:
         """
-        Extracts the solution from the final tableau.
+        Extracts the solution from the final table.
 
         Returns:
             A list of decision variable values.
@@ -111,11 +111,9 @@ class SimplexMethod:
         solution: list[float] = [0] * self.num_variables
         for i in range(self.num_constraints):
             for j in range(self.num_variables):
-                if self.tableau[i][j] == 1 and all(
-                    self.tableau[k][j] == 0
-                    for k in range(self.num_constraints)
-                    if k != i
+                if self.table[i][j] == 1 and all(
+                    self.table[k][j] == 0 for k in range(self.num_constraints) if k != i
                 ):
-                    solution[j] = self.tableau[i][-1]
+                    solution[j] = self.table[i][-1]
                     break
         return solution
